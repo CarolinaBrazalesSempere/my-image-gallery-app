400dea97bd8df519c90983bfb603855ba66fe8e6
CarolinaBrazalesSempere <carolbrazalessempere@gmail.com>
2021-02-27 11:30:18 +0100

    commit on master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 
 @NgModule({
